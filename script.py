83a96646a3d86775f92c2b596c18c96f62200c2a
Matt Vukojevic <matthew.vukojevic@gmail.com>
2017-10-23 23:41:47 -0400

    scrapes all photos from dataMiner.csv, outputs them into the profiles photo

--- script.py
+++ script.py
@@ -1,3 +1,20 @@
 import os
+import csv
 
-os.system('instagram-scraper ecolina97')
+file =  open ('DataMiner.csv', newline = '')
+reader = csv.reader(file)
+
+header = next(reader)
+
+profileNameList = []
+
+for row in reader:
+    profileName = (row[0])
+    profileNameList.append(profileName)
+
+
+os.system('cd profiles')
+os.system('instagram-scraper %s' % (profileNameList))
+
+
+#os.system('instagram-scraper {profilename}')
