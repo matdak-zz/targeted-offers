30d29d1847c2b2e62aaf536916dc39aca290f340
Matt Vukojevic <matthew.vukojevic@gmail.com>
2017-10-24 17:02:09 -0400

    working API calls to detect logos in images

--- script.py
+++ script.py
@@ -1,20 +1,66 @@
 import os
 import csv
+import io
+from google.cloud import vision
+
 
 file =  open ('DataMiner.csv', newline = '')
 reader = csv.reader(file)
-
 header = next(reader)
+vision_client = vision.Client()
 
+rootdir = '/users/mattvukojevic/documents/github/targeted-offers'
 profileNameList = []
+
 
 for row in reader:
     profileName = (row[0])
     profileNameList.append(profileName)
 
+os.chdir('profiles')
 
-os.system('cd profiles')
-os.system('instagram-scraper %s' % (str(profileNameList)[1:-1]))
+#os.system('instagram-scraper %s' % (str(profileNameList)[1:-1]))
 
 
-#os.system('instagram-scraper {profilename}')
+rootDir = '.'
+for dirName, subdirList, fileList in os.walk(rootDir):
+    filePath = os.path.join(rootDir, dirName)
+    os.chdir('%s' % filePath)
+    print(os.getcwd())
+    for fname in fileList:
+        print('\t%s' % fname)
+        with io.open(fname,'rb') as image_file:
+            content = image_file.read()
+            image = vision_client.image(content=content)
+            labels = image.detect_labels()
+            logos = image.detect_logos()
+
+            for label in labels:
+               print(label.description)
+
+
+
+    #
+    # profileNameFolder = subdirList
+    # print('Found directory: %s' % dirName)
+    # print(os.getcwd())
+    # os.chdir('%r' % (str(profileNameFolder)[2:-2]))
+    # for fname in fileList:
+    #     print(os.getcwd())
+        #with io.open(fname,'rb') as image_file:
+            #  #os.chdir(fname)
+            #  content = image_file.read()
+            #  image = vision_client.image(content=content)
+        #print('\t%s' % fname)
+
+# file_name = '500px-Guido_van_Rossum_OSCON_2006.jpg'
+#
+# with io.open(file_name,'rb') as image_file:
+#   content = image_file.read()
+#   image = vision_client.image(content=content)
+#
+# labels = image.detect_labels()
+# logos = image.detect_logos()
+#
+# for label in labels:
+#   print(label.description)
