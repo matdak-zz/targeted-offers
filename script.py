0122ea82095f68dfcb4be76bc2a74bb120b1bd30
Matt Vukojevic <matthew.vukojevic@gmail.com>
2017-10-25 23:16:09 -0400

    almost fully working downloader and text appender for labels. not ignoring instagram log file.

--- script.py
+++ script.py
@@ -19,55 +19,41 @@ for row in reader:
 
 os.chdir('profiles')
 
-os.system('instagram-scraper %s' % (str(profileNameList)[1:-1]))
+#os.system('instagram-scraper %s' % (str(profileNameList)[1:-1]))
 
 
 rootDir = '.'
 for dirName, subdirList, fileList in os.walk(rootDir):
-    print(dirName)
     filePath = os.path.join(rootDir, dirName)
+    print(len(subdirList))
+    n = 0
+    for subdirectory in subdirList:
+        savePath = os.path.join(rootDir, str(subdirList[n]))
+        completeTextFileName = os.path.join(savePath, str(subdirList[n])+".txt")
+        newTextFile = open(completeTextFileName, "w")
 
+    subdirList[:] = [d for d in subdirList if not d.startswith('.') and not d.endswith('.txt') and not d.startswith('.log')]
     print(os.getcwd())
-    subdirList[:] = [d for d in subdirList if not d.startswith('.') and not d.startswith('instagram-scraper')]
 
     for fname in fileList:
-        fileList[:] = [f for f in fileList if not f.startswith('.') and not f.startswith('instagram-scraper')]
+        fileList[:] = [f for f in fileList if not f.startswith('.') and not f.endswith('.log') and not f.endswith('.txt')]
 
         filePath = os.path.join(dirName, fname)
 
         print('\t%s' % filePath)
         with io.open(filePath,'rb') as image_file:
+
             content = image_file.read()
             image = vision_client.image(content=content)
             labels = image.detect_labels()
             logos = image.detect_logos()
 
+
             for label in labels:
                 print(label.description)
-
-
-
-    #
-    # profileNameFolder = subdirList
-    # print('Found directory: %s' % dirName)
-    # print(os.getcwd())
-    # os.chdir('%r' % (str(profileNameFolder)[2:-2]))
-    # for fname in fileList:
-    #     print(os.getcwd())
-        #with io.open(fname,'rb') as image_file:
-            #  #os.chdir(fname)
-            #  content = image_file.read()
-            #  image = vision_client.image(content=content)
-        #print('\t%s' % fname)
-
-# file_name = '500px-Guido_van_Rossum_OSCON_2006.jpg'
-#
-# with io.open(file_name,'rb') as image_file:
-#   content = image_file.read()
-#   image = vision_client.image(content=content)
-#
-# labels = image.detect_labels()
-# logos = image.detect_logos()
-#
-# for label in labels:
-#   print(label.description)
+                print(dirName)
+                print(os.getcwd())
+                labelDesc = label.description
+                path = '/users/mattvukojevic/documents/github/targeted-offers/profiles/%s/%s.txt' % (dirName[2:], dirName[2:])
+                with open (path, 'a') as f:
+                    f.write('%s, ' % labelDesc)
