215d2b4e9517b2c06a30f7f65ef78ac769210929
Matt Vukojevic <matthew.vukojevic@gmail.com>
2017-10-26 13:19:36 -0400

    completed script

--- script.py
+++ script.py
@@ -25,12 +25,7 @@ os.system('instagram-scraper %s' % (str(profileNameList)[1:-1]))
 rootDir = '.'
 for dirName, subdirList, fileList in os.walk(rootDir):
     filePath = os.path.join(rootDir, dirName)
-    print(len(subdirList))
     n = 0
-    for subdirectory in subdirList:
-        savePath = os.path.join(rootDir, str(subdirList[n]))
-        completeTextFileName = os.path.join(savePath, str(subdirList[n])+".txt")
-        newTextFile = open(completeTextFileName, "w")
     print(os.getcwd())
     fileList[:] = [f for f in fileList if not f.startswith('.') and not f.endswith('.log') and not f.endswith('.txt')]
 
@@ -56,39 +51,40 @@ for dirName, subdirList, fileList in os.walk(rootDir):
                 print(text.description)
                 textDesc = text.description
                 secondPath = '/users/mattvukojevic/documents/github/targeted-offers/profiles/%s/%s-text.txt' % (dirName[2:], dirName[2:])
-                with open (path, 'a') as f:
+                with open (secondPath, 'a') as f:
                     f.write('Text Detected: %s, ' % textDesc)
 
             for logo in logos:
                 print(logo.description)
                 logoDesc = logo.description
-                secondPath = '/users/mattvukojevic/documents/github/targeted-offers/profiles/%s/%s-text.txt' % (dirName[2:], dirName[2:])
-                with open (path, 'a') as f:
+                thirdPath = '/users/mattvukojevic/documents/github/targeted-offers/profiles/%s/%s-logos.txt' % (dirName[2:], dirName[2:])
+                with open (thirdPath, 'a') as f:
                     f.write('Logo Detected: %s, ' % logoDesc)
 
             for landmark in landmarks:
                 print(landmark.description)
                 landmarkDesc = landmark.description
-                secondPath = '/users/mattvukojevic/documents/github/targeted-offers/profiles/%s/%s-text.txt' % (dirName[2:], dirName[2:])
-                with open (path, 'a') as f:
+                fourthPath = '/users/mattvukojevic/documents/github/targeted-offers/profiles/%s/%s-landmarks.txt' % (dirName[2:], dirName[2:])
+                with open (fourthPath, 'a') as f:
                     f.write('Landmark Detected: %s, ' % landmarkDesc)
 
         newPath = '/users/mattvukojevic/documents/github/targeted-offers/profiles/%s/%s-labels.txt' % (dirName[2:], dirName[2:])
         t = open(newPath,'r')
         text = t.readline()
-
-        if 'travel' in text and travelCounter == 0:
-            imagePath = '/users/mattvukojevic/documents/github/targeted-offers/travel.jpg'
-            profileAdPath = '/users/mattvukojevic/documents/github/targeted-offers/profiles/%s' % dirName[2:]
-            shutil.copy2(imagePath, profileAdPath)
-            travelCounter += 1
-            with Image.open(imagePath) as img:
-                img.show()
+        print('%s' % dirName)
 
         if 'graduation' in text and graduationCounter == 0:
             imagePath = '/users/mattvukojevic/documents/github/targeted-offers/graduation.jpg'
             profileAdPath = '/users/mattvukojevic/documents/github/targeted-offers/profiles/%s' % dirName[2:]
             shutil.copy2(imagePath, profileAdPath)
             graduationCounter += 1
+            with Image.open(imagePath) as img:
+                img.show()
+
+        if 'travel' in text and travelCounter == 0:
+            imagePath = '/users/mattvukojevic/documents/github/targeted-offers/travel.jpg'
+            profileAdPath = '/users/mattvukojevic/documents/github/targeted-offers/profiles/%s' % dirName[2:]
+            shutil.copy2(imagePath, profileAdPath)
+            travelCounter += 1
             with Image.open(imagePath) as img:
                 img.show()
